cf13a206a4f4a7b65e80da6900c20e9a4cba64f2
Michał Terczyński <mterczynski1@gmail.com>
2024-01-19 17:48:51 +0100

    Setup working unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-	setupFiles: ['jest-canvas-mock']
+	preset: 'ts-jest/presets/js-with-ts',
+	runner: 'jest-electron/runner',
+	testEnvironment: 'jest-electron/environment',
+	setupFilesAfterEnv: [
+			'jest-extended/all',
+	],
 };
