64a3f187e6dd2ea8d30f3bb2045b96a86e8507e9
Michał Terczyński <mterczynski1@gmail.com>
2024-05-23 00:19:14 +0200

    Remove problematic jest-electron, remove pixi related unit tests

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 	preset: 'ts-jest/presets/js-with-ts',
-	runner: 'jest-electron/runner',
-	testEnvironment: 'jest-electron/environment',
 	setupFilesAfterEnv: [
-			'jest-extended/all',
+		'jest-extended/all',
 	],
 };
