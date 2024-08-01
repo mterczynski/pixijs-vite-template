364c902928cc14711893e1c5b51a29e112db8ed2
Michał Terczyński <mterczynski1@gmail.com>
2024-08-02 00:31:06 +0200

    Fix backround loading

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import { settings } from "./settings";
 
 export async function init() {
 	const app = await initializeApp();
-	const background = createBackground();
+	const background = await createBackground();
 
 	app.stage.addChild(background);
 
 	return app
 }
 
-function createBackground() {
+async function createBackground() {
+	await PIXI.Assets.load("/assets/metal-texture.webp")
 	const background = PIXI.Sprite.from("/assets/metal-texture.webp");
 	background.width = settings.canvasWidth;
 	background.height = settings.canvasHeight;
