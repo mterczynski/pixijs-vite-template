23daa1b9de953ac1c47ca0f3a04aeaa6827fe034
Michał Terczyński <mterczynski1@gmail.com>
2025-07-10 19:30:17 +0200

    Improve movement

--- src/player.ts
+++ src/player.ts
@@ -4,8 +4,9 @@ import { settings } from "./settings";
 
 export class Player extends PIXI.Container {
 	private sprite = PIXI.Sprite.from(assetList.pigButcher);
-	private moveDirection = 0;
 	private faceDirection = 1;
+	private leftPressed = false;
+	private rightPressed = false;
 
 	constructor() {
 		super();
@@ -15,28 +16,29 @@ export class Player extends PIXI.Container {
 
 		addEventListener("keydown", (e: KeyboardEvent) => {
 			if (e.key === "ArrowRight" || e.key.toLowerCase() === "d") {
-				this.moveDirection = 1;
-				this.faceDirection = 1;
+				this.rightPressed = true;
 			} else if (e.key === "ArrowLeft" || e.key.toLowerCase() === "a") {
-				this.moveDirection = -1;
-				this.faceDirection = -1;
+				this.leftPressed = true;
 			}
 		});
 
 		addEventListener("keyup", (e: KeyboardEvent) => {
-			if (
-				e.key === "ArrowRight" ||
-				e.key.toLowerCase() === "d" ||
-				e.key === "ArrowLeft" ||
-				e.key.toLowerCase() === "a"
-			) {
-				this.moveDirection = 0;
+			if (e.key === "ArrowRight" || e.key.toLowerCase() === "d") {
+				this.rightPressed = false;
+			}
+
+			if (e.key === "ArrowLeft" || e.key.toLowerCase() === "a") {
+				this.leftPressed = false;
 			}
 		});
 	}
 
 	update(ticker: PIXI.Ticker) {
-		this.x += this.moveDirection * ticker.deltaMS * settings.movementSpeed;
+		const moveDirection =
+			(this.leftPressed ? -1 : 0) + (this.rightPressed ? 1 : 0);
+		this.x += moveDirection * ticker.deltaMS * settings.movementSpeed;
+		this.faceDirection =
+			moveDirection > 0 ? 1 : moveDirection < 0 ? -1 : this.faceDirection;
 		this.scale.x = this.faceDirection;
 	}
 }
